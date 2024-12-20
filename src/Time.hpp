3c328811ad159bfeda371aa111140281cf70da1e
Vlad Savikin <vlad@savikin.ru>
2024-12-20 15:47:27 +0300

    Time.hpp: naming changes

--- src/Time.hpp
+++ src/Time.hpp
@@ -4,36 +4,38 @@
 #include <cstdint>
 #include <optional>
 
+/* Works with the hh:mm time format */
 struct Time {
-  int8_t h;
-  int8_t m;
+  int8_t hrs;
+  int8_t min;
 
-  static std::optional<Time> parse(const char *p) {
-    // isdigit from libc depends on locale (at least on msvc), hence this
-    static constexpr auto digit = [](char c) -> bool {
-      return '0' <= c && c <= '9';
+  static auto parse(const char *ptr) -> std::optional<Time> {
+    // isdigit from libc can depend on locale (at least on msvc), hence this
+    // (it shouldn't but it can)
+    static constexpr auto digit = [](char chr) -> bool {
+      return '0' <= chr && chr <= '9';
     };
-    if (!(digit(p[0])) || !(digit(p[1]))) {
+    if (!(digit(ptr[0])) || !(digit(ptr[1]))) {
       return std::nullopt;
     }
-    if (p[2] != ':') {
+    if (ptr[2] != ':') {
       return std::nullopt;
     }
-    if (!(digit(p[3])) || !(digit(p[4]))) {
+    if (!(digit(ptr[3])) || !(digit(ptr[4]))) {
       return std::nullopt;
     }
 
-    int8_t h;
-    int8_t m;
+    int8_t hrs;
+    int8_t min;
 
     // 01234
     // hh:mm
-    auto [p1, ec1] = std::from_chars(p + 0, p + 1, h);
-    auto [p2, ec2] = std::from_chars(p + 3, p + 4, h);
+    auto [p1, ec1] = std::from_chars(ptr + 0, ptr + 1, hrs);
+    auto [p2, ec2] = std::from_chars(ptr + 3, ptr + 4, hrs);
     if (ec1 != std::errc() || ec2 != std::errc()) {
       return std::nullopt;
     }
 
-    return Time{h, m};
+    return Time{.hrs = hrs, .min = min};
   }
 };
