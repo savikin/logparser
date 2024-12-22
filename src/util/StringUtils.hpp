6eee2cf99e510565547bc0c56231166b377f1855
Vlad Savikin <vlad@savikin.ru>
2024-12-22 13:20:54 +0300

    Mark StringUtils as inline
    As a header-only it shall warn about unused function otherwise.

--- src/util/StringUtils.hpp
+++ src/util/StringUtils.hpp
@@ -10,14 +10,14 @@
 
 namespace StringUtils {
 
-static auto stringsplit(const std::string &str) -> std::vector<std::string> {
+static inline auto stringsplit(const std::string &str) -> std::vector<std::string> {
   std::vector<std::string> tokens{};
   std::istringstream strstream{str};
   copy(std::istream_iterator<std::string>(strstream),
        std::istream_iterator<std::string>(), back_inserter(tokens));
   return tokens;
 }
-static auto atoi(const std::string &str) -> std::optional<int64_t> {
+static inline auto atoi(const std::string &str) -> std::optional<int64_t> {
   int64_t value;
   const char* cstr = str.c_str();
   auto [p1, ec1] = std::from_chars(cstr, cstr + str.size(), value);
